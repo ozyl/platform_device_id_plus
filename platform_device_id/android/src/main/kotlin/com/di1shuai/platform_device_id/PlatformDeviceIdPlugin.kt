e00a83a6f0accfb5ed2208243fe41e932796d4a6
Yongle <zylvip@163.com>
2025-02-10 12:04:41 +0800

    Removes references to Flutter v1 android embedding

--- platform_device_id/android/src/main/kotlin/com/di1shuai/platform_device_id/PlatformDeviceIdPlugin.kt
+++ platform_device_id/android/src/main/kotlin/com/di1shuai/platform_device_id/PlatformDeviceIdPlugin.kt
@@ -2,52 +2,51 @@ package com.di1shuai.platform_device_id
 
 import androidx.annotation.NonNull;
 
+
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.FlutterPlugin.FlutterPluginBinding
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
-/** PlatformDeviceIdPlugin */
-public class PlatformDeviceIdPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+/** Plugin method host for presenting a share sheet via Intent  */
+class PlatformDeviceIdPlugin : FlutterPlugin, ActivityAware,MethodCallHandler {
+  private lateinit var methodChannel: MethodChannel
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "platform_device_id")
-    channel.setMethodCallHandler(this);
+  override fun onAttachedToEngine(binding: FlutterPluginBinding) {
+    methodChannel = MethodChannel(binding.binaryMessenger, CHANNEL)
+    methodChannel.setMethodCallHandler(this)
   }
 
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
+  override fun onDetachedFromEngine(binding: FlutterPluginBinding) {
+    methodChannel.setMethodCallHandler(null)
+  }
+
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+  }
+
+  override fun onDetachedFromActivity() {
+  }
+
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    onAttachedToActivity(binding)
+  }
+
+  override fun onDetachedFromActivityForConfigChanges() {
+    onDetachedFromActivity()
+  }
+
   companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "platform_device_id")
-      channel.setMethodCallHandler(PlatformDeviceIdPlugin())
-    }
+    private const val CHANNEL = "platform_device_id"
   }
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+  override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
     if (call.method == "getPlatformVersion") {
       result.success("Android ${android.os.Build.VERSION.RELEASE}")
     } else {
       result.notImplemented()
     }
-  }
-
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
   }
 }
